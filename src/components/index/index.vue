92c15764f90f1bd138d896325b5efefdf9701f1a
fedeoo <xt_devil@sina.com>
2017-05-16 08:27:57 +1000

    init

---
+++ src/components/index/index.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    <sidebar></sidebar>
+
+  </div>
+
+
+</template>
+
+<script>
+import Sidebar from './sidebar'
+export default {
+  components:{Sidebar}
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/index/sidebar.vue
@@ -0,0 +1,58 @@
+<template lang="html">
+  <div class="phone-viewport">
+  <md-list>
+    <md-list-item>
+      <md-icon>whatshot</md-icon>
+      <span>News</span>
+
+      <md-list-expand>
+        <md-list>
+          <md-list-item class="md-inset">World</md-list-item>
+          <md-list-item class="md-inset">Americas</md-list-item>
+          <md-list-item class="md-inset">Europe</md-list-item>
+        </md-list>
+      </md-list-expand>
+    </md-list-item>
+
+    <md-list-item>
+      <md-icon>videogame_asset</md-icon>
+      <span>Games</span>
+
+      <md-list-expand>
+        <md-list>
+          <md-list-item class="md-inset">Console</md-list-item>
+          <md-list-item class="md-inset">PC</md-list-item>
+          <md-list-item class="md-inset">Phone</md-list-item>
+        </md-list>
+      </md-list-expand>
+    </md-list-item>
+
+    <md-list-item>
+      <md-icon>video_library</md-icon>
+      <span>Video</span>
+
+      <md-list-expand>
+        <md-list>
+          <md-list-item class="md-inset">Humor</md-list-item>
+          <md-list-item class="md-inset">Music</md-list-item>
+          <md-list-item class="md-inset">Movies</md-list-item>
+          <md-list-item class="md-inset">TV Shows</md-list-item>
+        </md-list>
+      </md-list-expand>
+    </md-list-item>
+
+    <md-list-item>
+      <md-icon>shopping_basket</md-icon>
+      <span>Shop</span>
+    </md-list-item>
+  </md-list>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
