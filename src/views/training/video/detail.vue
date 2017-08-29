34159d2262f326cd6126017bdf206a31600cfd92
Dan.Zhang <Dan.zhang@jimmys-iMac.local>
2017-08-29 16:13:50 +1000

    fix video

--- src/views/training/video/detail.vue
+++ src/views/training/video/detail.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
-		this is teaching detail.... id is{{ $route.query.id }}
-		<router-link to="?level=2&videoType=teaching">all teaching videos</router-link>
+		this is teaching detail.... id is  {{ $route.query.videoId }} --  {{ $route.query.videoType }}
+		<router-link to="?level=2&videoType=teaching">all {{ $route.query.videoType }} videos</router-link>
 		<router-link to="?level=1">all teaching videos</router-link>
 	</div>
 </template>
--- src/views/training/video/singleIndex.vue
+++ src/views/training/video/singleIndex.vue
@@ -1,8 +1,8 @@
 <template lang="html">
 <!--teaching 页面的首页-->
   <div>
-		this is teaching....{{ $route.query.videoType}}
-		<router-link to="?level=3&videoType=video-teaching-detail&videoId=233">detail</router-link>
+		this is all {{ $route.query.videoType}} videos
+		<router-link :to="'?level=3&videoType='+$route.query.videoType+'&videoId=233'">detail</router-link>
 		<router-link to="?level=1">all teaching videos</router-link>
 	</div>
 
