11dc70965a29ee652c53ade12f91c99b2adaab00
Li Li <LiLi@ACYCAPITAL.local>
2017-08-29 16:15:26 +1000

    test

--- src/router/map/training.js
+++ src/router/map/training.js
@@ -3,6 +3,7 @@ import onlineTraining from 'views/training/onlineTraining/index'
 import VideoAnalyse from 'views/training/video/analyse'
 import VideoStock from 'views/training/video/stock'
 import VideoTeaching from 'views/training/video/teaching'
+import Videos from 'views/training/video/index'
 
 import EducationalBooks from 'views/training/booksMagazines/educationalBooks'
 import moneyTheory from 'views/training/booksMagazines/moneyTheory'
@@ -28,6 +29,13 @@ export default [{
     requiresAuth: true
   }
 }, {
+  path: '/training/videos/index',
+  component: Videos,
+  meta: {
+    requiresAuth: true
+  },
+
+},{
   path: '/training/videos/stock',
   component: VideoStock,
   meta: {
