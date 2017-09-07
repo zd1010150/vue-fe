8718a67a23c7a7bf6bfad2623ce11ae36fa38c0b
Dan.Zhang <Dan.zhang@jimmys-iMac.local>
2017-09-07 14:16:06 +1000

    add one line

---
+++ src/components/ui/oneLine/index.js
@@ -0,0 +1,4 @@
+import oneLine from './oneLine'
+export default function install(Vue){
+	Vue.component('chp-one-line',oneLine)
+}
---
+++ src/components/ui/oneLine/oneLine.vue
@@ -0,0 +1,23 @@
+<template>
+	<span>
+		{{ innerContent }}
+	</span>
+</template>
+<script>
+	export default{
+		props:{
+			lens:Number,
+			content:String,
+			isHtml:false,
+			mark:{
+				type:String,
+				default: '...'
+			}
+		},
+		computed:{
+			innerContent(){
+				return this.content.substr(0,this.lens) + this.mark
+			}
+		}
+	}
+</script>
