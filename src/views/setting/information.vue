6841d914e2ad6b1e9c1d184645ba744e568604de
Dan.Zhang <Dan.zhang@jimmys-iMac.local>
2017-09-08 18:05:38 +1000

    add setting

---
+++ src/views/setting/information.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class='row'>
+		<div class="col-lg-12 col-sm-12">
+	        <header class="panel-heading bg-primary">
+	            <div class="widget-profile-info">
+	                <div class="profile-picture">
+	                    <img src="assets/images/!logged-user.jpg">
+	                </div>
+	                <div class="profile-info">
+	                    <h4 class="name text-weight-semibold pb-none">{{ }}</h4>
+	                    <h6 class="role">0405006066</h6>
+	                    <h5 class="role">will.bai@zerologix.com</h5>
+	                    <div class="profile-footer">
+	                        <a href="#" class="pull-left">Unit 49, 439-444 West Street, Hurstville, NSW, Australia 2220</a>
+	                    </div>
+	                </div>
+	            </div>
+	        </header>
+	        
+	        <div class="panel panel-accordion panel-accordion-first">
+	            <div class="panel-heading">
+	                <h4 class="panel-title">Company Information</h4>
+	            </div>
+	            <div class="accordion-body">
+	                <div class="panel-body">
+	                    <ul class="simple-user-list mb-xlg">
+	                        <li class="col-lg-4 col-md-6 col-sm-12">
+	                            <span class="message">Company Name</span>
+	                            <span class="title">Blue Shit</span>
+	                        </li>
+	                        <li class="col-lg-4 col-md-6 col-sm-12">
+	                            <span class="message">Country of Incorporation</span>
+	                            <span class="title">China</span>
+	                        </li>
+	                        <li class="col-lg-4 col-md-6 col-sm-12">
+	                            <span class="message">Business Registration Number</span>
+	                            <span class="title">666 666 666</span>
+	                        </li>
+	                        <li class="col-lg-4 col-md-6 col-sm-12">
+	                            <span class="message">Date of incorporation</span>
+	                            <span class="title">07/09/2017</span>
+	                        </li>
+	                        <li class="col-lg-4 col-md-6 col-sm-12">
+	                            <span class="message">Company Province</span>
+	                            <span class="title">Shandong</span>
+	                        </li>
+	                        <li class="col-lg-4 col-md-6 col-sm-12">
+	                            <span class="message">Company City</span>
+	                            <span class="title">Jinan</span>
+	                        </li>
+	                    </ul>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+</template>
---
+++ src/views/setting/subscribe.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="row">
+		<div class="col-sm-12">
+	        <div class="panel panel-accordion">
+	            <div class="panel-heading">
+	                <h4 class="panel-title">
+	                    <a class="accordion-toggle">
+	                        Subscribe/Unsubscribe Email
+	                    </a>
+	                </h4>
+	            </div>
+	            <div class="accordion-body">
+	                <div class="panel-body">
+	                    <div class="checkbox-custom checkbox-default col-lg-4 col-md-6 col-sm-12">
+	                        <input type="checkbox" checked="" id="todoListItem1" class="todo-check">
+	                        <label class="todo-label" for="todoListItem1">
+	                            <span>Lorem ipsum dolor sit amet</span>
+	                        </label>
+	                    </div>
+	                    <div class="checkbox-custom checkbox-default col-lg-4 col-md-6 col-sm-12">
+	                        <input type="checkbox" checked="" id="todoListItem1" class="todo-check">
+	                        <label class="todo-label" for="todoListItem1">
+	                            <span>Lorem ipsum dolor sit amet</span>
+	                        </label>
+	                    </div>
+	                    <div class="checkbox-custom checkbox-default col-lg-4 col-md-6 col-sm-12">
+	                        <input type="checkbox" checked="" id="todoListItem1" class="todo-check">
+	                        <label class="todo-label" for="todoListItem1">
+	                            <span>Lorem ipsum dolor sit amet</span>
+	                        </label>
+	                    </div>
+	                    <div class="clear-fix"></div>
+	                    <button type="button" class="btn btn btn-primary mr-xs btn-sm mt-sm">
+	                        <i class="fa fa-save mr-xs"></i> Save
+	                        <div class="chp-ink-ripple">
+	                            <div class="chp-ripple" style="width: 60px; height: 60px;"></div>
+	                        </div>
+	                    </button>
+
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+    </div>
+</template>
