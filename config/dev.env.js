55d14d55424ba9d718658a1fe3463f26d9841d8b
Dan.Zhang <Dan.zhang@jimmys-iMac.local>
2017-10-20 18:35:46 +1100

    modify the env

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,14 +1,8 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
-var tempet = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  __DEV__:true,
-  __PRO__:false
-});
+
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  __DEV__:true,
-  __PRO__:false
+  NODE_ENV: '"development"'
 })
