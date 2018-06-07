0e9e6f874218552852d25764788d8302843f2cee
ronanliu <ronan.liu@zerologix.com>
2018-06-07 16:48:05 +1000

    add config to enable video url configuration based on env

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -5,7 +5,10 @@
 // http://api.finlogixtest.com/v1/admin/login?email=steven.hu@zerologix.com&password=acy11111111&captcha=jh4hn
 // "http://api.cloudhubpanellocal.com/api/v1"
 
-let baseUrl = __API_URL__
+let baseUrl = __API_URL__,
+  videoBaseUrl = __VIDEO_URL__
+
 export {
-  baseUrl
+  baseUrl,
+  videoBaseUrl
 }
