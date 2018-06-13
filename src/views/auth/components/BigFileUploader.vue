35785def8b66aca71951b4eb449b52403bd9c45a
ronanliu <ronan.liu@zerologix.com>
2018-06-13 12:12:03 +1000

    add responsive to uploader part

--- src/views/auth/components/BigFileUploader.vue
+++ src/views/auth/components/BigFileUploader.vue
@@ -1,13 +1,18 @@
 <template>  
-  <form method="post" action="" @submit.prevent="onSubmit" style="overflow:auto">
+  <form method="post" action="" @submit.prevent="onSubmit">
     <!--组件最外部需要有一个名为aetherupload-wrapper的id，用以包装组件-->
-    <div class="form-group " id="aetherupload-wrapper" >
-      <div class="controls" >                  
-        <div class="uploader">
+    <div class="row">
+    <div class="form-group col-sm-6 " id="aetherupload-wrapper" >
+      <div class="controls">
+        <div class="uploader clearfix">
           <!--需要有一个名为file的id，用以标识上传的文件，aetherupload(file,group)中第二个参数为分组名，success方法可用于声名上传成功后的回调方法名-->
-          <input type="file" id="file" :disabled="inputDisabled"/>
-          <div class="progress mt-sm mb-none ">
+          <div class="col-sm-6">
+            <input type="file" id="file" :disabled="inputDisabled"/>
+          </div>
+          <div class="col-sm-6">
+          <div class="progress mb-none ">
             <div id="progressbar"></div><!--需要有一个名为progressbar的id，用以标识进度条-->
+          </div>
           </div>
         </div>     
         <span style="font-size:12px;color:#aaa;" id="output"></span><!--需要有一个名为output的id，用以标识提示信息-->
@@ -24,14 +29,16 @@
           class="formatTip error"
         >
           * 视频超出规定大小，请压缩您的视频
-        </div>                  
+        </div>
       </div>
     </div>
-    <div role="placeholder" class="mt-lg" />
-    <button type="submit" class="btn btn-primary" :disabled="hashedVideoId == -1">
-      <i class="fa fa-upload" aria-hidden="true"></i>
-      确认上传
-    </button>
+    <div class="col-sm-6">
+      <button type="submit" class="btn btn-primary" :disabled="hashedVideoId == -1">
+        <i class="fa fa-upload" aria-hidden="true"></i>
+        确认上传
+      </button>
+    </div>
+    </div>
   </form>
 </template>
 <script>
@@ -116,14 +123,19 @@
   
   #aetherupload-wrapper {
     .uploader {
-      padding: 1em 1.5em;
+      padding: .5em;
       background: @dark-bg-color;
+
+      input[type="file"] {
+        max-width: 100%;
+      }
     }
 
     .progress {
       background: #5c5f64;
       height: 10px;
       width: 100%;
+      margin-top: 7px;
 
       #progressbar {
         background: #3c87c7;
@@ -141,5 +153,14 @@
         font-weight: 700;
       }
     }
-  }  
+  }
+
+  @media (max-width: @screen-sm-min) {
+    #aetherupload-wrapper {
+      .uploader {
+        padding: 1em 1.5em;
+        background: @dark-bg-color;
+      }
+    }
+  }
 </style>
